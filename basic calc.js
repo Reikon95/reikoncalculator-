0dbc1854b8690a7c06eadba98906cccbaf4a3ea1
Reikon <38852603+Reikon95@users.noreply.github.com>
2018-05-17 12:42:45 +0100

    Update basic calc.js

--- basic calc.js
+++ basic calc.js
@@ -1,4 +1,16 @@
-var first = prompt("enter first number");
-var second = prompt("enter second number");
-var sum = Number(first) + Number(second);
-alert("The sum is: " + sum);
+var first = prompt("Enter first number");
+var operand = prompt ("Enter operand. At present only +, -, * or / can be accommodated");
+var second = prompt ("Enter second number");
+if (operand == '+'){
+    final = Number(first) + Number(second)
+} else if (operand == "-"){
+    final = Number(first) - Number(second)
+} else if (operand == "*"){
+    final = Number(first) * Number(second)
+} else if (operand == "/"){
+    final = Number(first) / Number(second)
+} else {
+    final = "Error"
+};
+alert(final);
+ 

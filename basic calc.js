ecebb5a8204ae9a0ade4c6a78bd38e6f3336f7ae
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-02-18 10:40:51 +0000

    Update basic calc.js

--- basic calc.js
+++ basic calc.js
@@ -1,6 +1,6 @@
-var first = prompt("Enter first number");
-var operand = prompt ("Enter operand. At present only +, -, * or / can be accommodated");
-var second = prompt ("Enter second number");
+let first = prompt("Enter first number");
+let operand = prompt ("Enter operand. At present only +, -, * or / can be accommodated");
+let second = prompt ("Enter second number");
 if (operand == '+'){
     final = Number(first) + Number(second)
 } else if (operand == "-"){
